827a72c7c43f2c9904f56f47c0276c09282dff40
Alson Shareef <alson.shareef@gmail.com>
2019-04-24 19:58:43 +0800

    Created function that dynamically determines the world dimensions based on the structure of the levels template string

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,18 @@
-import { buildLevel } from './levels';
+import { buildLevel, gridCalc, resizeCanvas, level } from './levels';
 import * as level_data from '../js/level_data.json';
 
 // Game variables
 let canvas = document.getElementById('primary-canvas'),
 	ctx = canvas.getContext('2d'),
-	grid = { width: 22, height: 8 };
+	world_grid = gridCalc(level); // Sets games world grid dimensions based on map data
 
 // DRAWING ELEMENTS
 const draw = () => {
+	// First, resize canvas based on grid aspect ratio to keep elements proportionate
+	// resizeCanvas(ctx.canvas, world_grid);
+
 	// Build out the level and receive an array of element objects.
-	let elements = buildLevel(level_data.map, canvas, grid);
+	let elements = buildLevel(level, canvas, world_grid);
 
 	// Draw out each element object using its properties
 	elements.forEach((e) => {
@@ -18,3 +21,5 @@ const draw = () => {
 };
 
 draw();
+
+// console.log(ctx.canvas.height / ctx.canvas.width);
--- src/js/levels.js
+++ src/js/levels.js
@@ -3,7 +3,8 @@ import { solidElement } from './elements';
 // ------
 
 // Static level string for testing.
-const level = `
+export const level = `
+......................
 ......................
 ..#................#..
 ..#................#..
@@ -12,17 +13,52 @@ const level = `
 ..#####............#..
 ......#............#..
 ......##############..
-......................`;
+......................
+......................
+`;
+
+// Translates template string to array data structure
+const levelArray = function(level) {
+	return level.trim().split('\n').map((row) => [ ...row ]);
+};
+
+// Grid calculator - Defines grid dimensions based on level template string
+export const gridCalc = (levelData) => {
+	let grid = {},
+		rows,
+		rowLengths;
+
+	// Calculates the amount of rows, and elements in each row
+	rows = levelArray(levelData).length;
+	rowLengths = levelArray(levelData)[0].length;
+
+	// Sets grid values based on rows and row lengths
+	grid.height = rows;
+	grid.width = rowLengths;
+
+	return grid;
+};
+
+// This will resize the canvas to have dimensions that will
+// export const resizeCanvas = (canvas, grid) => {
+// 	if (canvas.height / canvas.width > grid.height / grid.width) {
+// 		canvas.height = canvas.width * (grid.height / grid.width);
+// 		canvas.width = canvas.width;
+// 	} else {
+// 		canvas.height = canvas.height;
+// 		canvas.width = canvas.height / (grid.height / grid.width);
+// 	}
+// };
 
 // This function takes in the level string, and determines what needs to be built on the map at what size.
 export const buildLevel = (levelData, canvas, grid) => {
-	// Array that stores all element objects based on respective class constructors
+	// Array that stores all element objects that are created with respective class constructors
 	let finalLevelData = [];
 
 	// This will translate the 'level template string' into an array of row arrays containing elements
-	let rows = levelData.trim().split('\n').map((row) => [ ...row ]);
+	let rows = levelArray(levelData);
 
-	// This will iterate over every row array and determine what element needs to be drawn depending on each elements character.
+	// This will iterate over every row array and determine what element needs to be drawn depending on each element character.
 	rows.forEach((row, rowIndex) => {
 		row.forEach((element, elementIndex) => {
 			let position = {
