acb896a741f8e1c89f9e7b097652883d1920b0a1
Alson Shareef <alson.shareef@gmail.com>
2019-04-27 22:14:14 +0800

    Input class initializes event listeners and passes callbacks defined in the Map class

--- src/js/input.js
+++ src/js/input.js
@@ -1,16 +1,13 @@
 export default class Input {
-	constructor(callback, saveMap) {
-		document.getElementById('primary-canvas').addEventListener('click', (e) => {
-			let windowRect = e.target.getBoundingClientRect();
-			let clickCoordinates = {
-				x: e.clientX - windowRect.left,
-				y: e.clientY - windowRect.top
-			};
-			callback(clickCoordinates);
-		});
+	constructor() {}
 
-		document.addEventListener('keypress', (e) => {
-			saveMap(e);
-		});
+	handleClick(handleBlocks) {
+		document
+			.getElementById("primary-canvas")
+			.addEventListener("click", handleBlocks);
+	}
+
+	handleKeypress(handleLevelData) {
+		document.addEventListener("keypress", handleLevelData);
 	}
 }
