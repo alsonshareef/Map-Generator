687460fd7cd3677291d5e662226d2a00e7354312
Alson Shareef <alson.shareef@gmail.com>
2019-04-27 22:00:35 +0800

    Added @babel/plugin-proposal-class-properties to transform all static class properties and on-initialization properties

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -17,9 +17,15 @@ module.exports = {
 				}
 			},
 			{
-				test: /\.js$/,
-				loader: "babel-loader",
-				exclude: [/node_modules/]
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+						plugins: ["@babel/plugin-proposal-class-properties"]
+					}
+				}
 			}
 		]
 	}
