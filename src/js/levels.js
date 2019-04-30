d559eb6220fa9e053416ab0dc134ee78353f70c1
Alson Shareef <alson.shareef@gmail.com>
2019-04-30 20:20:05 +0800

    Added load, save and delete level data methods which look at static json files, localStorage and APIs to carry out their respective functionalities.

--- src/js/levels.js
+++ src/js/levels.js
@@ -1,35 +1,76 @@
 /**
  * File: levels.js
- * Description: Stores the levelService class which is responsible for receiving level data, and either distributing it or saving that data.
+ * Description: Stores the levelService class which is responsible for all kinds of actions regarding level data.
  */
 
-import * as level_data from "./level_data.json";
+import * as static_levelData from "./level_data.json";
 
 // ------
 
 export default class levelService {
-	// Default level from level data json file.
 	constructor() {
-		// console.log(level_data);
-		this.default_level = level_data.default.default;
-		this.test_level = level_data.default.test;
+		this.rest_levelData = {}; // level data stored in RESTful API.
 	}
 
-	// Will grab level data from either a json file or localStorage.
-	loadLevelData(levelName) {
-		switch (levelName) {
-			case "default":
-				return this.default_level;
-				break;
+	// LOAD
+	loadLevelData(loadType, levelName) {
+		// If requesting level data from static json file.
+		if (loadType === "static") {
+			document.querySelector(
+				".map-name"
+			).innerHTML = `MAP NAME: "${levelName}"`;
+			switch (levelName) {
+				case "default":
+					return static_levelData.default.default;
+					break;
 
-			case "test":
-				return this.test_level;
-				break;
-			default:
-				break;
+				case "test":
+					return static_levelData.default.test;
+					break;
+				default:
+					break;
+			}
 		}
+
+		// If requesting level data from local storage
+		if (loadType === "local") {
+			let levelData = JSON.parse(localStorage.getItem(levelName));
+			if (levelData) {
+				document.querySelector(
+					".map-name"
+				).innerHTML = `MAP NAME: "${levelName}"`;
+				return levelData;
+			} else {
+				alert(
+					`Sorry, there is no map in your storage under the name "${levelName}"`
+				);
+			}
+		}
+
+		// User requesting level data from REST API.
 	}
 
-	// Will save the level data to restful service or localStorage
-	saveLevelData(levelName, levelData) {}
+	// SAVE
+	saveLevelData(saveType, levelName, levelData) {
+		// If requesting a save to localStorage
+		if (saveType === "local") {
+			document.querySelector(
+				".map-name"
+			).innerHTML = `MAP NAME: "${levelName}"`;
+			localStorage.setItem(levelName, JSON.stringify(levelData));
+		}
+
+		// If requesting a save to REST API.
+	}
+
+	// DELETE
+	deleteLevelData(deleteType, levelName) {
+		// If requesting a delete from localStorage
+		if (deleteType === "local") {
+			localStorage.removeItem(levelName);
+		}
+		alert(`${levelName} was deleted.`);
+
+		// If requesting a delete from REST API.
+	}
 }
